ac186905d2a087e5ccbf038bf776c233f7efa3f5
nhat2012782 <gonbbb1@gmail.com>
2024-02-27 15:32:16 +0700

    add MUI lib

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { atom, useRecoilState } from 'recoil';
-import { textState } from '../store/recoil_state';
 import NavBar from '../components/NavBar';
+import { Typography } from '@mui/material';
 
 export default function Root() {
   const { isPending, error, data } = useQuery({
@@ -11,25 +9,12 @@ export default function Root() {
     queryFn: () =>
       axios.get('https://api.github.com/repos/tannerlinsley/react-query').then((res) => res.data),
   });
-  const [text, setText] = useRecoilState(textState);
   if (isPending) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
   return (
     <>
       <NavBar />
-      <button
-        className='p-4 mt-2 bg-red-200 rounded-full'
-        onClick={() => {
-          setText(text + 'a');
-        }}
-      >
-        Click here to add more letter A (test recoil)
-      </button>
-      {text}
-      <div>
-        <h2 className='text-2xl font-bold'>Below are data fetched from React Query Example</h2>
-        <pre className='m-2 border text-wrap'>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+      <Typography>Hello world</Typography>
     </>
   );
 }
