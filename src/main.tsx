f59563b25204f273ff90e5433ce9d3e1d7dad42b
nhat2012782 <gonbbb1@gmail.com>
2024-02-29 20:44:15 +0700

    remove all unused init

--- src/main.tsx
+++ src/main.tsx
@@ -5,38 +5,17 @@ import '@fontsource/roboto/700.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-  },
-  {
-    path: '/login',
-    element: <div>Login</div>,
-  },
-  {
-    path: '/sign-up',
-    element: <div>Sign up</div>,
-  },
-
-  {
-    path: '*',
-    element: <div>Sai link roi` Bro</div>,
-  },
-]);
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <RouterProvider router={router} />
+        <p>Hello World</p>
       </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>,
