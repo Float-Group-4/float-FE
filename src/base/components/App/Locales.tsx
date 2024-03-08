068e3e7394e445c42d2433ddd761b4e925362dcc
nhat2012782 <gonbbb1@gmail.com>
2024-03-08 23:43:27 +0700

    fix build error

--- src/base/components/App/Locales.tsx
+++ src/base/components/App/Locales.tsx
@@ -4,12 +4,8 @@ import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 
-import useConfig from '@base/hooks/useConfig';
-// import { useAppSetting } from '@base/hooks/user-setting/useAppSetting';
 import 'dayjs/locale/ko';
 import 'dayjs/locale/en';
-import { DefaultConfigProps } from '@base/types/config';
-import React from 'react';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -19,8 +15,6 @@ interface Props {
 }
 
 const Locales = ({ children }: Props) => {
-  const { initAppSetting } = useConfig();
-
   dayjs.tz.setDefault('Asia/Seoul');
 
   return <>{children}</>;
