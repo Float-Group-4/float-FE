014b988f085197ec7b7938d85affb857c26c1f3f
Minh Bao <bebaoboy@gmail.com>
2024-03-11 00:06:45 +0700

    finish 2 page

--- src/pages/PeoplePage/PeopleView/index.tsx
+++ src/pages/PeoplePage/PeopleView/index.tsx
@@ -1,84 +1,176 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+  Box,
+  Button,
+  Checkbox,
+  IconButton,
+  ListItemButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { grey } from '@mui/material/colors';
+import { createData } from './CustomTableProp';
+import CustomizedTables from './CustomizedTable';
+import { useState } from 'react';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
 
+// const CheckboxGroup = Checkbox.Group;
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
+const plainOptions = ['Active', 'Archived', 'AccountAccess'];
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(1, '1', 'Bảo Huỳnh Minh', 'Employee', 'IT', 'Editor', 'None', 'EB', 'Types'),
+  createData(2, '2', 'M', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+  createData(3, '3', 'A', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+  createData(4, '4', 'B', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+  createData(5, '5', 'PP', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+  createData(6, '6', 'M', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+  createData(7, '7', 'A', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+  createData(8, '8', 'B', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+  createData(9, '9', 'PP', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
 ];
 
-function CustomizedTables() {
+function CheckboxGroup() {
+  const [cb, setCb] = useState(false);
+  const [filterString, setFilterString] = useState(plainOptions[0]);
+  const [checkedList, setCheckedList] = useState([true, false, false]);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  // const [zoomModeAnchorEl, setZoomModeAnchorEl] = useState<null | HTMLElement>(null);
+  // const [densityAnchorEl, setDensityAnchorEl] = useState<null | HTMLElement>(null);
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onChange = (checked: boolean, index: number) => {
+    console.log('checkedList', checkedList);
+    checkedList[index] = checked;
+    setFilterString(
+      plainOptions
+        .filter((e, v) => checkedList[v])
+        .join(', ')
+        .toString(),
+    );
+    setCheckedList(checkedList);
+  };
+
+  // const onShowDropdown = () => {
+  //   setCb(!cb);
+  // };
+
+  const handleClickViewModeMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSelectViewMode = (_e: React.MouseEvent<HTMLElement>, index: number) => {
+    // setSelectedIndex(index);
+    // setAnchorEl(null);
+    checkedList[index] = !checkedList[index];
+    setFilterString(
+      plainOptions
+        .filter((e, v) => checkedList[v])
+        .join(', ')
+        .toString(),
+    );
+    setCheckedList(checkedList);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className='bg-white' >
+      <div className='flex gap-2 items-center ps-4'>
+        <IconButton aria-label='' onClick={() => {}} className='rounded-md hover:bg-white'>
+          <PersonAddAlt1OutlinedIcon sx={{ color: grey[800] }} />
+        </IconButton>
+        <ListItemButton
+          id='addBtn'
+          aria-controls='addMenu'
+          aria-expanded={cb ? 'true' : undefined}
+          onClick={handleClickViewModeMenu}
+          className={`rounded-md flex items-center h-9 aspect-square p-0  text-black  hover:bg-white focus:outline-none focus:bg-white   text-white`}
+          sx={{ border: 0, padding: 0, minWidth: 0 ,width: '300px'}}
+        >
+          <Typography className={` px-4 py-1 rounded-md decoration-dotted`} fontSize={18} sx={{textDecorationStyle: 'dotted', textDecorationColor: 'blue'}}>
+            {filterString}
+          </Typography>
+
+          <ExpandMoreOutlinedIcon fontSize='small' className='ms-2 color-black' color='primary' />
+        </ListItemButton>
+      </div>
+      <div>
+        <Menu
+          id='addMenu'
+          MenuListProps={{
+            'aria-labelledby': 'addBtn',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          className='top-2'
+        >
+          {plainOptions.map((value, index) => {
+            return (
+              <MenuItem
+                key={value}
+                selected={index === selectedIndex}
+                onClick={(event) => handleSelectViewMode(event, index)}
+                className={`flex justify-between min-w-56 rounded-md`}
+              >
+                <div className='flex items-center gap-8 min-h-8'>
+                  <Checkbox
+                    onChange={(e) => {
+                      onChange(e.target.checked, index);
+                    }}
+                    checked={checkedList[index]}
+                  />
+
+                  {value}
+                </div>
+                {/* {selectedIndex == index ? (
+                    <div>
+                      <CheckOutlinedIcon sx={{ fontSize: 16 }} />
+                    </div>
+                  ) : null} */}
+              </MenuItem>
+            );
+          })}
+        </Menu>
+      </div>
+    </div>
   );
 }
 
-
 export default function PeopleView() {
-  return <div className='bg-white flex-1 h-full'>People</div>;
+  return (
+    <div className='bg-white flex-1 h-full px-9 py-3'>
+      {CheckboxGroup()}
+      {CustomizedTables(rows)}
+      <Box sx={{ width: '100%' }} className='content-center flex flex-col items-center mx-0 mr-5'>
+        <Stack spacing={3} direction='column' className='flex content-center align-center'>
+          <div className='content-center' style={{ marginLeft: -20 }}>
+            <img src='/src/base/assets/imgs/people_tab.png' width={200}></img>
+          </div>
+          <Button
+            variant='outlined'
+            className='rounded-md bg-blue-600 text-white px-6 py-1'
+            sx={{ borderColor: grey[300], color: grey[800] }}
+          >
+            {/* <AddOutlinedIcon fontSize='small' className='' /> */}
+            Import your team
+          </Button>
+          <Typography className='underline text-grey text-center'>
+            <a href=''>No thanks</a>{' '}
+          </Typography>
+        </Stack>
+      </Box>
+    </div>
+  );
 }
--- src/pages/ProjectPage/ProjectView/index.tsx
+++ src/pages/ProjectPage/ProjectView/index.tsx
@@ -1,3 +1,171 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  IconButton,
+  ListItemButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { grey } from '@mui/material/colors';
+import { createData } from '@pages/PeoplePage/PeopleView/CustomTableProp';
+import CustomizedTables from '@pages/PeoplePage/PeopleView/CustomizedTable';
+import { useState } from 'react';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
+import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutlined';
+
+// const CheckboxGroup = Checkbox.Group;
+
+const plainOptions = ['Active', 'Archived', 'My projects'];
+
+const rows = [
+  createData(1, '1', 'Bảo Huỳnh Minh', 'Employee', 'IT', 'Editor', 'None', 'EB', 'Types'),
+  createData(2, '2', 'M', '__', 'IT', 'Account Owner', 'None', '__', 'Types'),
+
+];
+
+function CheckboxGroup() {
+  const [cb, setCb] = useState(false);
+  const [filterString, setFilterString] = useState(plainOptions[0]);
+  const [checkedList, setCheckedList] = useState([true, false, false]);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  // const [zoomModeAnchorEl, setZoomModeAnchorEl] = useState<null | HTMLElement>(null);
+  // const [densityAnchorEl, setDensityAnchorEl] = useState<null | HTMLElement>(null);
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onChange = (checked: boolean, index: number) => {
+    console.log('checkedList', checkedList);
+    checkedList[index] = checked;
+    setFilterString(
+      plainOptions
+        .filter((e, v) => checkedList[v])
+        .join(', ')
+        .toString(),
+    );
+    setCheckedList(checkedList);
+  };
+
+  // const onShowDropdown = () => {
+  //   setCb(!cb);
+  // };
+
+  const handleClickViewModeMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSelectViewMode = (_e: React.MouseEvent<HTMLElement>, index: number) => {
+    // setSelectedIndex(index);
+    // setAnchorEl(null);
+    checkedList[index] = !checkedList[index];
+    setFilterString(
+      plainOptions
+        .filter((e, v) => checkedList[v])
+        .join(', ')
+        .toString(),
+    );
+    setCheckedList(checkedList);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+
+  return (
+    <div className='bg-white' >
+      <div className='flex gap-2 items-center ps-4'>
+        <IconButton aria-label='' onClick={() => {}} className='rounded-md hover:bg-white'>
+          <CreateNewFolderOutlinedIcon sx={{ color: grey[800] }} />
+        </IconButton>
+        <ListItemButton
+          id='addBtn'
+          aria-controls='addMenu'
+          aria-expanded={cb ? 'true' : undefined}
+          onClick={handleClickViewModeMenu}
+          className={`rounded-md flex items-center h-9 aspect-square p-0  text-black  hover:bg-white focus:outline-none focus:bg-white   text-white`}
+          sx={{ border: 0, padding: 0, minWidth: 0 ,width: '300px'}}
+        >
+          <Typography className={` px-4 py-1 rounded-md decoration-dotted`} fontSize={18} sx={{textDecorationStyle: 'dotted', textDecorationColor: 'blue'}}>
+            {filterString}
+          </Typography>
+
+          <ExpandMoreOutlinedIcon fontSize='small' className='ms-2 color-black' color='primary' />
+        </ListItemButton>
+      </div>
+      <div>
+        <Menu
+          id='addMenu'
+          MenuListProps={{
+            'aria-labelledby': 'addBtn',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          className='top-2'
+        >
+          {plainOptions.map((value, index) => {
+            return (
+              <MenuItem
+                key={value}
+                selected={index === selectedIndex}
+                onClick={(event) => handleSelectViewMode(event, index)}
+                className={`flex justify-between min-w-56 rounded-md`}
+              >
+                <div className='flex items-center gap-8 min-h-8'>
+                  <Checkbox
+                    onChange={(e) => {
+                      onChange(e.target.checked, index);
+                    }}
+                    checked={checkedList[index]}
+                  />
+
+                  {value}
+                </div>
+                {/* {selectedIndex == index ? (
+                    <div>
+                      <CheckOutlinedIcon sx={{ fontSize: 16 }} />
+                    </div>
+                  ) : null} */}
+              </MenuItem>
+            );
+          })}
+        </Menu>
+      </div>
+    </div>
+  );
+}
+
 export default function ProjectView() {
-  return <div className='bg-white flex-1 h-full'>Project</div>;
+  return (
+    <div className='bg-white flex-1 h-full px-9 py-3'>
+      {CheckboxGroup()}
+      {CustomizedTables(rows)}
+      <Box sx={{ width: '100%' }} className='content-center flex flex-col items-center mx-0 mr-5'>
+        <Stack spacing={3} direction='column' className='flex content-center align-center'>
+          <div className='content-center' style={{ marginLeft: -20 }}>
+            <img src='/src/base/assets/imgs/project_tab.png' width={200}></img>
+          </div>
+          <Button
+            variant='outlined'
+            className='rounded-md bg-blue-600 text-white px-6 py-1'
+            sx={{ borderColor: grey[300], color: grey[800] }}
+          >
+            {/* <AddOutlinedIcon fontSize='small' className='' /> */}
+            Import your projects
+          </Button>
+          <Typography className='underline text-grey text-center'>
+            <a href=''>No thanks</a>{' '}
+          </Typography>
+        </Stack>
+      </Box>
+    </div>
+  );
 }
