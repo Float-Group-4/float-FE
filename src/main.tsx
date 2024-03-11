45af2bcaa6799ad467875ffff6a9151b4b8f1ecd
nien11102002 <nguyenduynien11102002@gmail.com>
2024-03-12 02:31:53 +0700

    Progess: LandingPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@base/components/App/App';
+import LandingPage from '@pages/LandingPage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
 );
