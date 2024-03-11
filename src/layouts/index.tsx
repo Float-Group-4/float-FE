98a66cf11e20f1de98ee5066f4cd1c5b88f90d1c
nhat2012782 <gonbbb1@gmail.com>
2024-03-11 22:19:09 +0700

    fix layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,13 +5,16 @@ import { CONTAINER_PADDING_X, HEADER_HEIGHT } from '@base/config/constants';
 
 import AuthLayout from './AuthLayout';
 import NotAuthLayout from './NotAuthLayout';
+import { useMatch } from 'react-router-dom';
 
 interface LayoutsProps {}
 
 const Layouts = (props: LayoutsProps) => {
   // change condition login here
-  const isLogin = true;
-  return <>{isLogin ? <AuthLayout /> : <NotAuthLayout />}</>;
+  const isSignIn = useMatch('sign-in');
+  const isSignUp = useMatch('sign-up');
+
+  return <>{isSignIn || isSignUp ? <NotAuthLayout /> : <AuthLayout />}</>;
 };
 
 export default Layouts;
